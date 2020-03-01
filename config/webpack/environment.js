f78a3692d71f0247ce7533a4e7c31adde2a6054e
tk0358 <tk0358a@yahoo.co.jp>
2020-03-01 21:05:55 +0900

    configuration for sending email

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,3 +9,13 @@ environment.plugins.append("Provide", new webpack.ProvidePlugin({
 }))
 
 module.exports = environment
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
